4e155ae567a5bbd7ec9d57ad37581f30fa61fb95
Marius Cotofana <marius.coto@gmail.com>
2014-10-26 23:01:41 +0200

    Show joined challenges on profile page; allow admins to join challenge

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,8 @@
-var mongoose = require('mongoose');
-var Users    = mongoose.model('Users');
-var Notifications   = mongoose.model('Notifications');
-var core 		= require('../core.js');
+var mongoose      = require('mongoose');
+var Users         = mongoose.model('Users');
+var Challenges    = mongoose.model('Challenges');
+var Notifications = mongoose.model('Notifications');
+var core 		      = require('../core.js');
 
 
 /*
@@ -32,25 +33,43 @@ exports.edit = function(req, res) {
 User profile page. Shows all info about selected user.
 */
 exports.index = function(req, res) {
+  var _self = {}
   var cname = req.url.substring(1, (req.url + '/').substring(1).indexOf('/')+1);
   var uid = ((req.session.auth) ? req.session.auth.github.user.id : null);
 
   Users.findOne ({ 'user_name': cname }, function(err, cuser) {
     if (!cuser) return res.status(404).render('404', {title: "404: File not found"})
     else {
+      Users.findOne({'user_id': uid}, function(err, user) {
 
-      Users.findOne ({ 'user_id': uid }, function(err, user) {
-
-        res.render('profile', {
-          title:      cuser.user_fullname,
-          currentUrl: '',
-          cuser: 	    cuser,
-          user: 		  user
-        });
-
+        _self.user = user
+        _self.cuser = cuser
+        Challenges.find().exec(gotChallenges)
       })
     }
   })
+
+  function gotChallenges(err, challenges) {
+    // User joined some challenges
+    _self.joined = false
+
+    // Mark challenges joined by current user
+    for (var ch in challenges) {
+      if (challenges[ch].users.indexOf(_self.cuser.user_name) > -1) {
+        challenges[ch].joined = true
+        _self.joined = true
+      }
+    }
+
+    res.render('profile', {
+      'title':      _self.cuser.user_fullname,
+      'currentUrl': '',
+      'challenges': challenges,
+      'cuser':      _self.cuser,
+      'user':       _self.user,
+      'joined':     _self.joined
+    });
+  }
 }
 
 /*
