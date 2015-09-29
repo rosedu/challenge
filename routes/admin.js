0dd190d610d6d58843eecedf3fa0f7940979932f
catalin-oancea <catalin.oancea2008@gmail.com>
2015-09-30 02:38:29 +0300

    Inform the end user about the error

--- routes/admin.js
+++ routes/admin.js
@@ -32,17 +32,38 @@ exports.index = function(req, res) {
 Add info from form to db.
 */
 exports.challenge_add = function(req, res) {
-
   // Add all admins in list even if they do not exist
   new Challenges({
     name:         req.body.name,
     link:         req.body.name.replace(/\s+/g, ''),
     description:  req.body.description,
     admins:       req.body.admins.split(' ')
-  }).save(savedChallenge);
+  }).save(function(err){
+    //if err -> render admin page again
+    if(err)
+    {
+      var _self = {};
+      var uid = ((req.session.auth) ? req.session.auth.github.user.id : null);
 
-  function savedChallenge(err, todo, count) {
-    console.log("* Challenge " + req.body.name + " saved.");
-    res.redirect('/challenges');
-  }
+      Users.find().exec(gotAll);
+
+      function gotAll(err, all) {
+        _self.users = all;
+        Users.findOne({'user_id': uid}).exec(gotUser);
+      }
+
+      function gotUser(err, user) {
+        res.render('admin', {
+          'title':  'New challenge',
+          'user':   user,
+          'users':  _self.users,
+          'error': 'Challenge name should be unique.',
+          'description': req.body.description,
+          'admins': req.body.admins
+        });
+      }
+    }
+    else
+      res.redirect('/challenges');
+  });
 };
